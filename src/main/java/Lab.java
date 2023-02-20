d3fe12b8562a892bcc13d5261f0a11175e5bc5f2
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-20 21:31:40 +0000

    lab completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,26 +1,35 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 /**
- * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game "Towers of Hanoi".
- * They are used in algorithms where it is needed to keep track of the ordering in which the elements were inserted.
+ * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game 
+ "Towers of Hanoi".
+ * They are used in algorithms where it is needed to keep track of the ordering in which the 
+ elements were inserted.
  *
- * ArrayDeque fills the role of *both* queues and stacks. You could think of a Deque as a deck of cards (pronounced the
+ * ArrayDeque fills the role of *both* queues and stacks. You could think of a Deque as a deck 
+ *  cards (pronounced the
  * same) where you could remove cards from either the top or bottom. You can use ArrayDeque
  * to perform stack operations. You can read about ArrayDeque in the official Javadocs here:
  * https://docs.oracle.com/javase/7/docs/api/java/util/ArrayDeque.html
- * And you can read about the stack class here (deque exhibits the same behavior as Stack and contains the same
+ * And you can read about the stack class here (deque exhibits the same behavior as Stack and 
+ * contains the same
  * methods): https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
  */
 public class Lab {
     /**
-     * Implement and return some class that implements Deque, such as ArrayDeque, which contains all the functionality
-     * of a Stack. This challenge will not be using the Stack class, because the Stack class is known to have a shoddy
-     * implementation as it hasn't been changed since the very earliest version of Java. Deques were made to exhibit
+     * Implement and return some class that implements Deque, such as ArrayDeque, which contains
+     *  all the functionality
+     * of a Stack. This challenge will not be using the Stack class, because the Stack class is 
+     * known to have a shoddy
+     * implementation as it hasn't been changed since the very earliest version of Java. Deques 
+     * were made to exhibit
      * the exact same behavior and are generally better.
      *
      * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
      */
     public Deque<String> createNewStack(){
-        return null;
+        Deque<String> stack = new ArrayDeque<>();
+        return stack;
     }
 
     /**
@@ -28,7 +37,7 @@ public class Lab {
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        return 0;
+        return stack.size();
     }
 
     /**
@@ -38,6 +47,7 @@ public class Lab {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
+        stack.push(item);
     }
 
     /**
@@ -47,7 +57,7 @@ public class Lab {
      * @return the value popped from the top of the deque.
      */
     public String removeFromTopOfStack(Deque<String> stack){
-        return "";
+        return stack.pop();
     }
     /**
      * Peek the value at the top of a deque.
@@ -57,6 +67,6 @@ public class Lab {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        return stack.peek();
     }
 }
